76e5d50e0f62a8a6ec43503b3067a0888b3e52df
Aaron Ten Clay <aarontc@aarontc.com>
2015-06-23 15:20:42 -0700

    Prettified AggregateChannel.inspect and added date_type_id

--- lib/tdms/aggregate_channel.rb
+++ lib/tdms/aggregate_channel.rb
@@ -6,20 +6,34 @@ module TDMS
 			@channels = channels
 		end
 
+
+		def inspect
+			"#<#{self.class.name}:#{self.object_id} path=#{path.inspect}, #{@channels.length} channel(s)>"
+		end
+
+
 		def path
 			@channels[0].path
 		end
+
 
 		def name
 			@channels[0].name
 		end
 
+
 		def data_type
 			@channels[0].data_type
 		end
 
+
+		def data_type_id
+			@channels[0].data_type_id
+		end
+
+
 		def values
-			@values ||= AggregateChannelEnumerator.new(@channels)
+			@values ||= AggregateChannelEnumerator.new @channels
 		end
 	end
 end
